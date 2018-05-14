2903c8e34795f20839fcd68fed2ab1ded22f4814
aricallen <aric.allen2@gmail.com>
2018-05-14 08:09:53 -0700

    auto transition to dashboard

--- app/application/route.js
+++ app/application/route.js
@@ -1,7 +1,7 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  // beforeModel() {
-  //   this.transitionTo('dashboard');
-  // },
+  afterModel() {
+    this.transitionTo('dashboard');
+  },
 });
