399569752e6000c9042e4488f655b2438a0d7803
aricallen <aric.allen2@gmail.com>
2018-05-15 11:04:55 -0700

    fix mirage env bug

--- app/services/ticker.js
+++ app/services/ticker.js
@@ -1,6 +1,7 @@
 import Service, { inject } from '@ember/service';
 import { get, set } from '@ember/object';
 import { later } from '@ember/runloop';
+import { isNone } from '@ember/utils';
 
 export default Service.extend({
   store: inject(),
@@ -16,9 +17,13 @@ export default Service.extend({
   startPolling(timeout = 5000) {
     this.update();
 
-    const timer = later(this, this.startPolling, timeout);
-    set(this, 'timer', timer);
-    return timer;
+    const timer = get(this, 'timer');
+    if (isNone(timer) === false) {
+      const newTimer = later(this, this.startPolling, timeout);
+      set(this, 'timer', newTimer);
+      return newTimer;
+    }
+    return null;
   },
 
   stopPolling() {
