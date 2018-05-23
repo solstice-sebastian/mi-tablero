53c8d6ba698a1a2f1d2bba2377254301faeefedc
aricallen <aric.allen2@gmail.com>
2018-05-23 08:17:11 -0700

    better spinner placement

--- mirage/config.js
+++ mirage/config.js
@@ -1,3 +1,4 @@
+import { later } from '@ember/runloop';
 import dashboardMock from './mocks/dashboard';
 import fetch from './mocks/tickers';
 import ENV from '../config/environment';
@@ -5,7 +6,14 @@ import ENV from '../config/environment';
 export default function() {
   this.urlPrefix = ENV.VARS.API_HOST;
   this.namespace = '';
-  this.get('/dashboards', () => dashboardMock);
+  // this.get('/dashboards', () => dashboardMock);
+  this.get(
+    '/dashboards',
+    () =>
+      new Promise((res) => {
+        later(() => res(dashboardMock), 1000 * 60 * 60);
+      })
+  );
   this.get('/tickers', () => fetch());
   this.post('/notifications');
   // this.passthrough('/notifications');
