c89a89bee2cf723011afe28de93acbbdaad6e2a3
aricallen <aric.allen2@gmail.com>
2018-05-23 08:21:16 -0700

    default side and status values

--- mirage/config.js
+++ mirage/config.js
@@ -3,15 +3,17 @@ import dashboardMock from './mocks/dashboard';
 import fetch from './mocks/tickers';
 import ENV from '../config/environment';
 
+// const DELAY_TIME = 1000 * 60 * 60;
+const DELAY_TIME = 5;
+
 export default function() {
   this.urlPrefix = ENV.VARS.API_HOST;
   this.namespace = '';
-  // this.get('/dashboards', () => dashboardMock);
   this.get(
     '/dashboards',
     () =>
       new Promise((res) => {
-        later(() => res(dashboardMock), 1000 * 60 * 60);
+        later(() => res(dashboardMock), DELAY_TIME);
       })
   );
   this.get('/tickers', () => fetch());
